2aadb0fe41c119b3f83cbec9a9af3fecff554f5b
Ali Hatami <a.hatam008@gmail.com>
2023-03-16 17:42:41 +0330

    Update extract test

--- test/util/test_buffer.py
+++ test/util/test_buffer.py
@@ -32,6 +32,7 @@ class AllBufferTest(unittest.TestCase):
         self.assertEqual(self.buf.extract(), "void")
         self.assertEqual(self.buf.forward, 4)
         self.assertEqual(self.buf.beginning, 4)
+        self.assertEqual(self.buf(), " ")
 
     def test_extract_retreat(self):
         pass
