7a94a38adcf769a024b51a0d2c2fc623a0e06042
FatemehAlimoradi <f.alimoradi80@gmail.com>
2023-03-17 21:20:37 +0330

    fix file saving

--- src/util/logger.py
+++ src/util/logger.py
@@ -55,20 +55,24 @@ class Logger:
             return "There is no lexical error."
         return errors_string
 
-    def save_as_text(self, string: str, file_name: str):
-        f = open(file_name, "w")
-        f.write(string)
-        f.close()
+    def save_as_text(self, string: str, file_name=None, file=None):
+        if file_name != None:
+            f = open(file_name, "w")
+            f.write(string)
+            f.close()
+        else:
+            file.write(string)
+            file.close()
 
     def create_log(self, symbol_table, file_tokens=None, file_errors=None, file_symbols=None):
         if file_symbols == None:
-            self.save_as_text(self.create_errors_string(self.errors), "lexical_errors.txt")
-            self.save_as_text(self.create_tokens_string(self.tokens), "tokens.txt")
-            self.save_as_text(self.create_symbol_table_string(symbol_table), "symbol_table.txt")
+            self.save_as_text(self.create_errors_string(self.errors), file_name="lexical_errors.txt")
+            self.save_as_text(self.create_tokens_string(self.tokens), file_name="tokens.txt")
+            self.save_as_text(self.create_symbol_table_string(symbol_table), file_name="symbol_table.txt")
         else:
-            self.save_as_text(self.create_errors_string(self.errors), file_errors)
-            self.save_as_text(self.create_tokens_string(self.tokens), file_tokens)
-            self.save_as_text(self.create_symbol_table_string(symbol_table), file_symbols)
+            self.save_as_text(self.create_errors_string(self.errors), file = file_errors)
+            self.save_as_text(self.create_tokens_string(self.tokens), file = file_tokens)
+            self.save_as_text(self.create_symbol_table_string(symbol_table), file = file_symbols)
 
     def add_error(self, cur_line_no, lexim, tt):
         if len(lexim) > 6:
