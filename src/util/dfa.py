3037c6963aaec2922025bc08485757998aac32a9
Ali Hatami <a.hatam008@gmail.com>
2023-03-17 13:39:16 +0330

    Fix dfa __call__ returns None

--- src/util/dfa.py
+++ src/util/dfa.py
@@ -29,7 +29,7 @@ class Dfa:
 
     def __call__(self, *args, **kwds) -> Tuple[TokenType, bool]:
         """dfa(buffer) is equivalent of dfa.match(buffer)"""
-        self.match(args[0])
+        return self.match(args[0])
 
     def match(self, buffer) -> Tuple[TokenType, bool]:
         """accepts input
