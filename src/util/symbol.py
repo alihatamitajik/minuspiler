a7608b794767bdbb239a4db890a89f1e563fbf10
Ali Hatami <a.hatam008@gmail.com>
2023-06-26 18:28:04 +0330

    Add semantic symbol structure

--- src/util/symbol.py
+++ src/util/symbol.py
@@ -1,6 +1,45 @@
 from dataclasses import dataclass
-from enum import Enum
+
+
+@dataclass
+class SemanticSymbol:
+    """Symbol class that is returned by get_by_id
+
+    This class contains name, addr and weather it's a global (that should be
+    used directly as its address), relative local address (that should be
+    used with TOP_SP address) or a constant.
+
+    These addresses should be indexed and used at runtime so its better to have
+    a dedicated function for conversions and using them.
+
+    Note: is_pointer flag is intended to be useful when we pass address of first
+    element of an array and it might not be used (I'm not sure at this moment).
+    """
+    name: str
+    value: int
+    is_global: bool
+    is_constant: bool
+    is_pointer: bool
 
 
 class SymbolTable:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def install_func(self, name, return_type):
+        pass
+
+    def end_func(self):
+        pass
+
+    def add_arg_to_func(self, name):
+        pass
+
+    def install_variable(self, name, type):
+        pass
+
+    def install_array(self, name, type, size):
+        pass
+
+    def get_by_id(self, id):
+        pass
